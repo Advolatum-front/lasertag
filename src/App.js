da14e61406182c81dc1aa0c922f77d70aad266b9
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-23 16:18:35 +0500

    move footer to separated component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
@@ -20,7 +21,7 @@ function App() {
             <Route exact path="/page2" element={<Page2 />} />
           </Routes>
         </main>
-        <footer className="footer"></footer>
+        <Footer />
       </BrowserRouter>
     </>
   );
