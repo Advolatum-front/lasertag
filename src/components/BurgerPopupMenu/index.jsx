b612617aba93c46ea0af0d9290ff37d1e3dc74bb
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-23 13:40:44 +0500

    turn popup into fullscreen one and add close button

--- src/components/BurgerPopupMenu/index.jsx
+++ src/components/BurgerPopupMenu/index.jsx
@@ -1,4 +1,5 @@
 import { createPortal } from "react-dom";
+import { Link } from "react-router-dom";
 
 import "./index.css";
 
@@ -7,7 +8,40 @@ const popupContainer = document.getElementById("popup");
 const BurgerPopupMenu = (props) => {
   const { open, onClosePopupMenu } = props;
   const popupMenu = (
-    <div className="pop" onClick={() => onClosePopupMenu()}></div>
+    <nav className="header__burger-popup-menu">
+      <button
+        type="button"
+        className="header__close-popup-menu"
+        onClick={() => onClosePopupMenu()}
+      ></button>
+      <ul className="header__popup-nav-list">
+        <li>
+          <Link to="/" className="header__popp-nav-link">
+            Документы
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="header__popp-nav-link">
+            Новости
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="header__popp-nav-link">
+            Мероприятия
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="header__popp-nav-link">
+            Галерея
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="header__popp-nav-link">
+            Контакты
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 
   return open ? createPortal(popupMenu, popupContainer) : null;
