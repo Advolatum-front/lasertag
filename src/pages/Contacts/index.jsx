ec0a9f47f0d7b74abb2a7bedb47f09416c617e0a
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-24 00:43:08 +0500

    basic contacts page is almost completed

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,7 +1,34 @@
+import presidentPhoto from "../../img/contacts/president.webp";
+
 import "./index.css";
 
 const Contacts = () => {
-  return <p>контакты</p>;
+  return (
+    <>
+      <article className="contacts">
+        <div className="contacts__baddge">
+          <img src={presidentPhoto} alt="" className="contacts__photo" />
+          <div className="contacts__info">
+            <p className="contacts__status">
+              Председатель Регионального отделения Всероссийской общественной
+              организации развития лазерного боя «Федерация Лазертага в
+              Санкт-Петербурге
+            </p>
+
+            <div className="contacts__fullname">РЯБЕНКО ДЕНИС ВИКТОРОВИЧ</div>
+            <ul className="contacts__list">
+              <li>
+                <a href="tel:+79999999999">89999999999</a>
+              </li>
+              <li>
+                <a href="mailto:lazertagooo@gmail.com">lazertagooo@gmail.com</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </article>
+    </>
+  );
 };
 
 export default Contacts;
