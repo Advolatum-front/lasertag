1b442344633ee6be767aa3e1615ff4ddb42236cb
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-24 13:12:46 +0500

    complete basic layout

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -9,19 +9,26 @@ const Contacts = () => {
         <div className="contacts__baddge">
           <img src={presidentPhoto} alt="" className="contacts__photo" />
           <div className="contacts__info">
-            <p className="contacts__status">
+            <div className="contacts__status">
               Председатель Регионального отделения Всероссийской общественной
               организации развития лазерного боя «Федерация Лазертага в
               Санкт-Петербурге
-            </p>
+            </div>
 
             <div className="contacts__fullname">РЯБЕНКО ДЕНИС ВИКТОРОВИЧ</div>
             <ul className="contacts__list">
               <li>
-                <a href="tel:+79999999999">89999999999</a>
+                <a href="tel:+79999999999" className="contacts__list-link">
+                  8 (999) 999-99-99
+                </a>
               </li>
               <li>
-                <a href="mailto:lazertagooo@gmail.com">lazertagooo@gmail.com</a>
+                <a
+                  href="mailto:lazertagooo@gmail.com"
+                  className="contacts__list-link"
+                >
+                  lazertagooo@gmail.com
+                </a>
               </li>
             </ul>
           </div>
