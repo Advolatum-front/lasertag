4915ed93548c99b10d27442b66f802db5ad594b7
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-25 03:01:21 +0500

    add document item layout with no styles so far

--- src/pages/Documents/index.jsx
+++ src/pages/Documents/index.jsx
@@ -3,7 +3,26 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 const Documents = () => {
-  return <article className="documents-container">Документы</article>;
+  return (
+    <article className="documents-container">
+      <ul className="documents-container__list">
+        <li className="documents-container__list-item">
+          <div className="documents-container__doc-item-1">
+            <div className="documents-container__doc-header">
+              Устав организации
+            </div>
+            <div className="documents-container__doc-description">
+              Краткое описание устава организации с некоторыми подробностями для
+              масштабности текста
+            </div>
+            <Link to="/" className="documents-container__doc-link">
+              Читать подробнее
+            </Link>
+          </div>
+        </li>
+      </ul>
+    </article>
+  );
 };
 
 export default Documents;
