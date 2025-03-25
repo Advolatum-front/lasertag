c6b1dd4cd7f6206e3194b72b38cda8e75bdd8082
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-25 14:00:53 +0500

    parametrized documentBlock component

--- src/components/DocumentBlock/index.jsx
+++ src/components/DocumentBlock/index.jsx
@@ -3,14 +3,20 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 const DocumentBlock = (props) => {
+  const {
+    headerClass,
+    headerContent,
+    descriptionClass,
+    descriptionContent,
+    link,
+    linkClass,
+  } = props;
+
   return (
     <div className="document-block">
-      <div className="document-block__header">Устав организации</div>
-      <div className="document-block__description">
-        Краткое описание устава организации с некоторыми подробностями для
-        масштабности текста
-      </div>
-      <Link to="/" className="document-block__link">
+      <div className={headerClass}>{headerContent}</div>
+      <div className={descriptionClass}>{descriptionContent}</div>
+      <Link to={link} className={linkClass}>
         Читать подробнее
       </Link>
     </div>
--- src/pages/Documents/index.jsx
+++ src/pages/Documents/index.jsx
@@ -7,7 +7,15 @@ const Documents = () => {
     <article className="documents-container">
       <ul className="documents-container__list">
         <li className="documents-container__list-item">
-          <DocumentBlock />
+          <DocumentBlock
+            headerContent="Устав организации"
+            headerClass="document-block__header"
+            descriptionClass="document-block__description"
+            descriptionContent="Краткое описание устава организации с некоторыми подробностями для
+        масштабности текста"
+            link="/"
+            linkClass="document-block__link"
+          />
         </li>
       </ul>
     </article>
