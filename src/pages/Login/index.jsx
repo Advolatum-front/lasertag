684b09eecffa32e0c5b7762b00a89616dd92877a
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-27 02:20:56 +0500

    make basic layout

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -12,18 +12,21 @@ const Login = () => {
   return (
     <div className="login-wrapper">
       <form className="login-wrapper__form" onSubmit={submitForm}>
-        <div className="login-wrapper__form-header">Восстановление пароля</div>
-        <div className="login-wrapper__form-tip">
-          Введите адрес электронной почты, связанный с вашим аккаунтом, и мы
-          вышлем вам ссылку для изменения пароля
-        </div>
+        <div className="login-wrapper__form-header">Вход</div>
         <LabeledInput type="email" label="E-mail" required />
+        <LabeledInput type="password" label="Пароль" required />
+
+        <div className="login-wrapper__links">
+          <Link to="/restorepassword" className="login-wrapper__link-item">
+            Забыли пароль
+          </Link>
+          <Link to="/registration" className="login-wrapper__link-item">
+            Нет аккаунта?
+          </Link>
+        </div>
         <button type="submit" className="login-wrapper__submit">
-          Сбросить пароль
+          Войти
         </button>
-        <Link to="/" className="login-wrapper__link">
-          Я вспомнил(а) пароль
-        </Link>
       </form>
       <div className="login-wrapper__bg"></div>
     </div>
