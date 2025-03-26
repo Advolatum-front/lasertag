6a8eb9cf93127fff214809005c881f4334a3b82d
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-26 15:11:28 +0500

    add basic layout without styles

--- src/pages/RestorePassword/index.jsx
+++ src/pages/RestorePassword/index.jsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+
+import LabeledInput from "../../components/controls/LabeledInput";
+
 import "./index.css";
 
 const RestorePassword = () => {
-  return <p>Прив</p>;
+  const submitForm = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="restore-wrapper">
+      <form className="restore-wrapper__form" onSubmit={submitForm}>
+        <div className="restore-wrapper__form-header">
+          Восстановление пароля
+        </div>
+        <div className="restore-wrapper__form-tip">
+          Введите адрес электронной почты, связанный с вашим аккаунтом, и мы
+          вышлем вам ссылку для изменения пароля
+        </div>
+        <LabeledInput type="email" label="E-mail" required />
+        <button type="submit" className="restore-wrapper__submit">
+          Отправить
+        </button>
+        <Link to="/">Я вспомнил(а) пароль</Link>
+      </form>
+      <div className="restore-wrapper__bg"></div>
+    </div>
+  );
 };
 
 export default RestorePassword;
