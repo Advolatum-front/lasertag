c5d14e177d6b495be71a4c6873b31a567d4e5300
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-28 22:55:16 +0500

    add basic layout

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,7 +1,51 @@
+import { Link } from "react-router-dom";
+
+import LabeledInput from "../../components/controls/LabeledInput";
+
 import "./index.css";
 
 const Registration = () => {
-  return <p>Регистр</p>;
+  const submitForm = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="registration">
+      <form className="registration__form" onSubmit={submitForm}>
+        <div className="registration__form-header">Регистрация</div>
+        <div className="registration__fields">
+          <LabeledInput required type="text" label="Имя" id="name" />
+          <LabeledInput required type="text" label="Фамилия" id="surname" />
+          <LabeledInput required type="text" label="Страна" id="country" />
+          <LabeledInput
+            required
+            type="text"
+            label="Населённый пункт"
+            id="city"
+          />
+          <LabeledInput
+            required
+            type="date"
+            label="Дата рождения"
+            id="birthdate"
+          />
+          <LabeledInput required type="email" label="Email" id="email" />
+          <LabeledInput required type="text" label="Телефон" id="phone" />
+          <LabeledInput required type="password" label="Пароль" id="password" />
+          <LabeledInput
+            required
+            type="password"
+            label="Повторите пароль"
+            id="passwordconfirm"
+          />
+          <LabeledInput required type="text" label="ААА" id="аааа" />
+        </div>
+        <button type="submit" className="registration__submit-form">
+          Зарегистрироваться
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Registration;
