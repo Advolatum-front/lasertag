444e8474c587786906c99de71327bc8c800f164c
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-29 21:06:40 +0500

    add basic layout without styles

--- src/pages/AlbumSelector/index.jsx
+++ src/pages/AlbumSelector/index.jsx
@@ -1,7 +1,55 @@
 import { Link } from "react-router-dom";
 
+// import "./index.css";
+
 const AlbumSelector = () => {
-  return <p>Альбом селектор</p>;
+  return (
+    <>
+      <ul className="gallery-navigator">
+        <li className="gallery-navigator__list-item">
+          <Link className="gallery-navigator__link">Фото</Link>
+        </li>
+        <li className="gallery-navigator__list-item">
+          <Link className="gallery-navigator__link">Видео</Link>
+        </li>
+        <li className="gallery-navigator__list-item">
+          <Link className="gallery-navigator__link">Избранное</Link>
+        </li>
+      </ul>
+      <ul className="albums-list">
+        <li className="albums-list__item">
+          <Link className="albums-list__cover-container">
+            <img
+              src="/img/gallery/album-selector/photo/cover-1.webp"
+              alt=""
+              className="albums-list__cover"
+            />
+          </Link>
+          <Link className="albums-list__link">Фото с региона</Link>
+        </li>
+        <li className="albums-list__item">
+          <Link className="albums-list__cover-container">
+            <img
+              src="/img/gallery/album-selector/photo/cover-1.webp"
+              alt=""
+              className="albums-list__cover"
+            />
+          </Link>
+          <Link className="albums-list__link">Фото с региона</Link>
+        </li>
+        <li className="albums-list__item">
+          <Link className="albums-list__cover-container">
+            <img
+              src="/img/gallery/album-selector/photo/cover-1.webp"
+              alt=""
+              className="albums-list__cover"
+            />
+          </Link>
+          <Link className="albums-list__link">Фото с региона</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default AlbumSelector;
