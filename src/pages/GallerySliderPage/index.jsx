05b3a2f3dce14cfbb163b1f42a831afefdec9841
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-31 00:42:04 +0500

    include slider with basic config

--- src/pages/GallerySliderPage/index.jsx
+++ src/pages/GallerySliderPage/index.jsx
@@ -4,10 +4,40 @@
 
 // import GalleryNavigator from "../../components/GalleryNavigator";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 import "./index.css";
 
 const GallerySliderPage = () => {
-  return <p>тут слайдер</p>;
+  return (
+    <>
+      <Swiper
+        slidesPerView={2}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 };
 
 export default GallerySliderPage;
