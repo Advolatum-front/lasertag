a20e79733c85b73ccc1c9f6c17d126c68e188170
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-30 15:58:44 +0500

    add basic layout

--- src/pages/AlbumContent/index.jsx
+++ src/pages/AlbumContent/index.jsx
@@ -1,11 +1,81 @@
 import { Link } from "react-router-dom";
 
+import GalleryNavigator from "../../components/GalleryNavigator";
+
 import "./index.css";
 
 const AlbumContent = () => {
   return (
     <>
-      <p>альбом-контент</p>
+      <GalleryNavigator className="gallery-navigator-mb" />
+      <Link to="/" className="back-to-albums-link">
+        Фото с региона
+      </Link>
+      <ul className="thumbnail-list">
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-2.jpg"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-3.jpg"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-4.jpg"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-5.jpg"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-6.jpg"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+        <li className="thumbnail-list__list-item">
+          <Link to="/" className="thumbnail-list__thumbnail-container">
+            <img
+              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-7.jpg"
+              alt=""
+              className="thumbnail-list__thumbnail"
+            />
+          </Link>
+        </li>
+      </ul>
     </>
   );
 };
