b378026a5970acb4745e4c9243d7ac76cedbb39d
Advolatum-front <Advolatum-front@yandex.ru>
2025-03-31 22:00:49 +0500

    add basic layout with styles

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -11,31 +11,88 @@ import "./index.css";
 const News = () => {
   return (
     <section className="news">
-      <h1 className="news__header">Новости</h1>
+      <h1 className="news__section-header">Новости</h1>
       <Swiper
         slidesPerView={3}
-        spaceBetween={30}
-        loop={true}
+        spaceBetween={2}
+        loop={false}
         navigation={true}
         modules={[Navigation]}
         className="news__slider"
-      ></Swiper>
-      <SwiperSlide className="news__news-item">
-        <div className="news__news-header"></div>
-        <Link className="news__news-image-container">
-          <img
-            src="/news/small/news-1.webp"
-            alt=""
-            className="news__news-image"
-          />
-        </Link>
-        <div className="news__news-date">21.05</div>
-        <div className="news__news-announce">
-          С 20-21 мая прошли отборочные региональные соревнования по лазерному
-          бою.
-        </div>
-        <Link className="news__news-link">Подробнее</Link>
-      </SwiperSlide>
+      >
+        <SwiperSlide className="news__news-item">
+          <div className="news__news-wrapper">
+            <div className="news__news-header">Региональные соревнования</div>
+            <Link className="news__news-image-container">
+              <img
+                src="/news/small/news-1.webp"
+                alt=""
+                className="news__news-image"
+              />
+            </Link>
+            <div className="news__news-date">21.05</div>
+            <div className="news__news-announce">
+              С 20-21 мая прошли отборочные региональные соревнования по
+              лазерному бою.
+            </div>
+            <Link className="news__news-link">Подробнее</Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="news__news-item">
+          <div className="news__news-wrapper">
+            <div className="news__news-header">Региональные соревнования</div>
+            <Link className="news__news-image-container">
+              <img
+                src="/news/small/news-1.webp"
+                alt=""
+                className="news__news-image"
+              />
+            </Link>
+            <div className="news__news-date">21.05</div>
+            <div className="news__news-announce">
+              С 20-21 мая прошли отборочные региональные соревнования по
+              лазерному бою.
+            </div>
+            <Link className="news__news-link">Подробнее</Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="news__news-item">
+          <div className="news__news-wrapper">
+            <div className="news__news-header">Региональные соревнования</div>
+            <Link className="news__news-image-container">
+              <img
+                src="/news/small/news-1.webp"
+                alt=""
+                className="news__news-image"
+              />
+            </Link>
+            <div className="news__news-date">21.05</div>
+            <div className="news__news-announce">
+              С 20-21 мая прошли отборочные региональные соревнования по
+              лазерному бою.
+            </div>
+            <Link className="news__news-link">Подробнее</Link>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="news__news-item">
+          <div className="news__news-wrapper">
+            <div className="news__news-header">Региональные соревнования</div>
+            <Link className="news__news-image-container">
+              <img
+                src="/news/small/news-1.webp"
+                alt=""
+                className="news__news-image"
+              />
+            </Link>
+            <div className="news__news-date">21.05</div>
+            <div className="news__news-announce">
+              С 20-21 мая прошли отборочные региональные соревнования по
+              лазерному бою.
+            </div>
+            <Link className="news__news-link">Подробнее</Link>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
