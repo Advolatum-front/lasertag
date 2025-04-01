c68a60fc9cbfa133d6c5c79c4d52d02be4398713
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-01 18:27:44 +0500

    complete adaptive

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -11,6 +11,76 @@ import "swiper/css/navigation";
 import "./index.css";
 
 const News = () => {
+  const newsData = [
+    {
+      id: 1,
+      header: "Региональные совренования",
+      photo: "/news/small/news-1.webp",
+      date: "21.05",
+      announce:
+        "С 20-21 мая прошли отборочные региональные соревнования по лазерному бою.",
+      link: "/",
+    },
+    {
+      id: 2,
+      header: "Региональные совренования",
+      photo: "/news/small/news-2.webp",
+      date: "21.05",
+      announce:
+        "С 20-21 мая прошли отборочные региональные соревнования по лазерному бою.",
+      link: "/",
+    },
+    {
+      id: 3,
+      header: "Региональные совренования",
+      photo: "/news/small/news-3.webp",
+      date: "21.05",
+      announce:
+        "С 20-21 мая прошли отборочные региональные соревнования по лазерному бою.",
+      link: "/",
+    },
+    {
+      id: 4,
+      header: "Региональные совренования",
+      photo: "/news/small/news-4.webp",
+      date: "21.05",
+      announce:
+        "С 20-21 мая прошли отборочные региональные соревнования по лазерному бою.",
+      link: "/",
+    },
+    {
+      id: 5,
+      header: "Региональные совренования",
+      photo: "/news/small/news-5.webp",
+      date: "21.05",
+      announce:
+        "С 20-21 мая прошли отборочные региональные соревнования по лазерному бою.",
+      link: "/",
+    },
+  ];
+
+  const newsCards = newsData.map((newsItem) => {
+    const { id, header, photo, date, announce, link } = newsItem;
+
+    return (
+      <div className="news__news-card" key={id}>
+        <div className="news__card-header">{header}</div>
+        <Link to={link}>
+          <img src={photo} alt="" className="news__card-image" />
+        </Link>
+        <div className="news__card-date">{date}</div>
+        <div className="news__card-announce">{announce}</div>
+        <Link to="/" className="news__card-link">
+          Подробнее
+        </Link>
+      </div>
+    );
+  });
+
+  const slides = newsCards.map((card, index) => {
+    return <SwiperSlide key={-(index + 1)}>{card}</SwiperSlide>;
+  });
+
   const breakpoints = {
     960: {
       slidesPerView: 2,
@@ -29,6 +99,7 @@ const News = () => {
   return (
     <section className="news">
       <h1 className="news__section-header">Новости</h1>
+      <div className="news__vertical-news-container">{newsCards}</div>
       <Swiper
         slidesPerView={1}
         spaceBetween={10}
@@ -48,96 +119,7 @@ const News = () => {
           swiper.navigation.update();
         }}
       >
-        <SwiperSlide>
-          <div className="news__news-card">
-            <div className="news__card-header">Региональные соревнования</div>
-            <Link>
-              <img
-                src="/news/small/news-1.webp"
-                alt=""
-                className="news__card-image"
-              />
-            </Link>
-            <div className="news__card-date">21.05</div>
-            <div className="news__card-announce">
-              С 20-21 мая прошли отборочные региональные соревнования по
-              лазерному бою.
-            </div>
-            <Link className="news__card-link">Подробнее</Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="news__news-card">
-            <div className="news__card-header">Региональные соревнования</div>
-            <Link>
-              <img
-                src="/news/small/news-1.webp"
-                alt=""
-                className="news__card-image"
-              />
-            </Link>
-            <div className="news__card-date">21.05</div>
-            <div className="news__card-announce">
-              С 20-21 мая прошли отборочные региональные соревнования по
-              лазерному бою.
-            </div>
-            <Link className="news__card-link">Подробнее</Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="news__news-card">
-            <div className="news__card-header">Региональные соревнования</div>
-            <Link>
-              <img
-                src="/news/small/news-1.webp"
-                alt=""
-                className="news__card-image"
-              />
-            </Link>
-            <div className="news__card-date">21.05</div>
-            <div className="news__card-announce">
-              С 20-21 мая прошли отборочные региональные соревнования по
-              лазерному бою.
-            </div>
-            <Link className="news__card-link">Подробнее</Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="news__news-card">
-            <div className="news__card-header">Региональные соревнования</div>
-            <Link>
-              <img
-                src="/news/small/news-1.webp"
-                alt=""
-                className="news__card-image"
-              />
-            </Link>
-            <div className="news__card-date">21.05</div>
-            <div className="news__card-announce">
-              С 20-21 мая прошли отборочные региональные соревнования по
-              лазерному бою.
-            </div>
-            <Link className="news__card-link">Подробнее</Link>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="news__news-card">
-            <div className="news__card-header">Региональные соревнования</div>
-            <Link>
-              <img
-                src="/news/small/news-1.webp"
-                alt=""
-                className="news__card-image"
-              />
-            </Link>
-            <div className="news__card-date">21.05</div>
-            <div className="news__card-announce">
-              С 20-21 мая прошли отборочные региональные соревнования по
-              лазерному бою.
-            </div>
-            <Link className="news__card-link">Подробнее</Link>
-          </div>
-        </SwiperSlide>
+        {slides}
       </Swiper>
       <div className="custom-navigation">
         <button className="custom-prev">
