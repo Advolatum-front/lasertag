f6eb65eac05c76930f3825ae4aa2cf7f6f72521b
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-01 13:28:09 +0500

    add breakpoints

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -11,12 +11,23 @@ import "swiper/css/navigation";
 import "./index.css";
 
 const News = () => {
+  const breakpoints = {
+    960: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+  };
+
   return (
     <section className="news">
       <h1 className="news__section-header">Новости</h1>
       <Swiper
-        slidesPerView={3}
-        spaceBetween={1}
+        slidesPerView={1}
+        spaceBetween={10}
         loop={false}
         navigation={{
           nextEl: ".custom-next",
@@ -30,7 +41,26 @@ const News = () => {
           swiper.navigation.init();
           swiper.navigation.update();
         }}
+        breakpoints={breakpoints}
       >
+        <SwiperSlide>
+          <div className="news__news-card">
+            <div className="news__card-header">Региональные соревнования</div>
+            <Link>
+              <img
+                src="/news/small/news-1.webp"
+                alt=""
+                className="news__card-image"
+              />
+            </Link>
+            <div className="news__card-date">21.05</div>
+            <div className="news__card-announce">
+              С 20-21 мая прошли отборочные региональные соревнования по
+              лазерному бою.
+            </div>
+            <Link className="news__card-link">Подробнее</Link>
+          </div>
+        </SwiperSlide>
         <SwiperSlide>
           <div className="news__news-card">
             <div className="news__card-header">Региональные соревнования</div>
