7d41ab6e1fa88d13e2b8cadd783f6b6bc8979f44
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-02 11:45:54 +0500

    add basic layout

--- src/pages/NewsItemPage/index.jsx
+++ src/pages/NewsItemPage/index.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 import "./index.css";
 
 const NewsItemPage = () => {
-  return <p>Выбранная новость</p>;
+  return (
+    <section className="current-news-section">
+      <div className="news-block">
+        <Link to="/" className="news-block__link-prev"></Link>
+        <img src="/news/big/photo-1.jpg" alt="" className="news-block__photo" />
+        <div className="news-block__info">
+          <h1 className="news-block__header">Региональные соревнования</h1>
+          <p className="news-block__text"></p>
+          <div className="news-block__footer">
+            <div className="news-block__additional-text">
+              Именно эта команда и пойдёт дальше отстаивать первенство лазерного
+              боя. Поздравим «Лунных зайцев»!
+            </div>
+            <Link to="/" className="news-block__link-prev"></Link>
+          </div>
+        </div>
+      </div>
+      <div className="current-news-date">21.05</div>
+    </section>
+  );
 };
 
 export default NewsItemPage;
