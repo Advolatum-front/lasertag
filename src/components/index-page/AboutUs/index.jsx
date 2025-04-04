c2b40a2c9f3c53c25edb197ae1672de0823b76ab
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-04 13:06:15 +0500

    add basic layout

--- src/components/index-page/AboutUs/index.jsx
+++ src/components/index-page/AboutUs/index.jsx
@@ -1,7 +1,32 @@
 import "./index.css";
 
 const AboutUs = () => {
-  return <p>щ нас</p>;
+  return (
+    <section className="about-us">
+      <div className="about-us__girl"></div>
+      <div className="about-us__text-block">
+        <h2 className="about-us__header">О нас</h2>
+        <div className="about-us__gun"></div>
+        <ul className="about-us__features-list">
+          <li className="about-us__feature-item">
+            <h2 className="about-us__feature-header">Наименование</h2>
+            <p className="about-us__feature-description">
+              Региональное отделение Всероссийской общественной организации
+              развития лазерного боя «Федерация Лазертага России» в
+              Санкт-Петербурге.
+            </p>
+          </li>
+          <li className="about-us__feature-item">
+            <h2 className="about-us__feature-header">Деятельность</h2>
+            <p className="about-us__feature-description">
+              Занимаемся организацией и проведением турниров, спортивно-массовых
+              мероприятий и дружеских встреч на территории Санкт-Петербурга.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default AboutUs;
