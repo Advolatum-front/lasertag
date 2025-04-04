bac1a40a39deb00bd87fd52dc1341f9ecec863e2
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-05 02:44:59 +0500

    style almost done

--- src/components/index-page/OurPartners/index.jsx
+++ src/components/index-page/OurPartners/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 const OurPartners = () => {
   return (
     <section className="our-partners">
-      <h2 className="our-parners__header">Наши партнёры</h2>
+      <h2 className="our-partners__header">Наши партнёры</h2>
       <ul className="our-partners__list">
         <li className="our-partners__list-item">
           <div className="partners-sprite partners-sprite-assembly"></div>
