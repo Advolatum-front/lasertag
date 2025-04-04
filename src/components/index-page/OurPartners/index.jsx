505d9b09142dff211ca6d208bb5e4e818c7e5ac8
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-05 01:42:36 +0500

    add basic layout

--- src/components/index-page/OurPartners/index.jsx
+++ src/components/index-page/OurPartners/index.jsx
@@ -1,7 +1,85 @@
 import "./index.css";
 
 const OurPartners = () => {
-  return <section className="our-partners">Партнёры</section>;
+  return (
+    <section className="our-partners">
+      <h2 className="our-parners__header">Наши партнёры</h2>
+      <ul className="our-partners__list">
+        <li className="our-partners__list-item">
+          <div className="partners-sprite partners-sprite-assembly"></div>
+          <div className="our-partners__text">
+            <div className="our-partners__partner-name">
+              Молодёжный парламент Санкт-Петербурга
+            </div>
+            <div className="our-partners__partner-description">
+              Будущее Петербурга – в руках молодёжи! Определи вектор развития
+              нашего города!
+            </div>
+          </div>
+        </li>
+        <li className="our-partners__list-item">
+          <div className="partners-sprite partners-sprite-assembly"></div>
+          <div className="our-partners__text">
+            <div className="our-partners__partner-name">
+              Молодёжный парламент Санкт-Петербурга
+            </div>
+            <div className="our-partners__partner-description">
+              Будущее Петербурга – в руках молодёжи! Определи вектор развития
+              нашего города!
+            </div>
+          </div>
+        </li>
+        <li className="our-partners__list-item">
+          <div className="partners-sprite partners-sprite-assembly"></div>
+          <div className="our-partners__text">
+            <div className="our-partners__partner-name">
+              Молодёжный парламент Санкт-Петербурга
+            </div>
+            <div className="our-partners__partner-description">
+              Будущее Петербурга – в руках молодёжи! Определи вектор развития
+              нашего города!
+            </div>
+          </div>
+        </li>
+        <li className="our-partners__list-item">
+          <div className="partners-sprite partners-sprite-assembly"></div>
+          <div className="our-partners__text">
+            <div className="our-partners__partner-name">
+              Молодёжный парламент Санкт-Петербурга
+            </div>
+            <div className="our-partners__partner-description">
+              Будущее Петербурга – в руках молодёжи! Определи вектор развития
+              нашего города!
+            </div>
+          </div>
+        </li>
+        <li className="our-partners__list-item">
+          <div className="partners-sprite partners-sprite-assembly"></div>
+          <div className="our-partners__text">
+            <div className="our-partners__partner-name">
+              Молодёжный парламент Санкт-Петербурга
+            </div>
+            <div className="our-partners__partner-description">
+              Будущее Петербурга – в руках молодёжи! Определи вектор развития
+              нашего города!
+            </div>
+          </div>
+        </li>
+        <li className="our-partners__list-item">
+          <div className="partners-sprite partners-sprite-assembly"></div>
+          <div className="our-partners__text">
+            <div className="our-partners__partner-name">
+              Молодёжный парламент Санкт-Петербурга
+            </div>
+            <div className="our-partners__partner-description">
+              Будущее Петербурга – в руках молодёжи! Определи вектор развития
+              нашего города!
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default OurPartners;
