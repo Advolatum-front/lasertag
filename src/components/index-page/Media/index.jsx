5efd5f0a798d82982140d0cd0f466283dfceff79
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-05 23:34:55 +0500

    add basic layout and styles

--- src/components/index-page/Media/index.jsx
+++ src/components/index-page/Media/index.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 import "./index.css";
 
 const Media = () => {
-  return <article className="media">медиа</article>;
+  return (
+    <article className="media">
+      <Link to="/gallery" className="media__link">
+        <span>Все медиа</span>
+      </Link>
+    </article>
+  );
 };
 
 export default Media;
