705f37acdef805f2d9c87287dd23ac49e4ac8fb3
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-10 23:36:42 +0500

    add styles

--- src/components/popups/Confirm/index.jsx
+++ src/components/popups/Confirm/index.jsx
@@ -8,19 +8,18 @@ const popupContainer = document.getElementById("popup");
 
 const Confirm = () => {
   const confirm = (
-    <div className="confirm-popup-overlay">
+    <div className="confirm-popup-overlay opened">
       <div className="confirm-popup">
         <button type="button" className="confirm-popup__button-close">
           <Cross />
         </button>
-        <p className="confirm-popup__text">Вы точно хотите?</p>
+        <p className="confirm-popup__text">
+          Вы точно хотите Вы точно хотите Вы точно хотите Вы точно хотите Вы
+          точно хотите Вы точно хотите Вы точно хотите ?
+        </p>
         <div className="confirm-popup__buttons">
-          <button type="button" className="confirm-popup__button-yes">
-            Да
-          </button>
-          <button type="button" className="confirm-popup__button-no">
-            Нет
-          </button>
+          <button type="button">Да</button>
+          <button type="button">Нет</button>
         </div>
       </div>
     </div>
