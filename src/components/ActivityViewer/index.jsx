3ded557e8123fb6c00e4630d2d777312bee05095
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-11 14:06:18 +0500

    parametrize activity viewer

--- src/components/ActivityViewer/index.jsx
+++ src/components/ActivityViewer/index.jsx
@@ -4,31 +4,22 @@ import { ReactComponent as Arrow } from "../../svg/arrow.svg";
 
 import "./index.css";
 
-const ActivityViewer = () => {
+const ActivityViewer = (props) => {
+  const { goBackLink, header, description, date, sendRequestLink } = props;
+
   return (
     <div className="activity-viewer">
-      <Link to="/" className="activity-viewer__link-go-back">
+      <Link to={goBackLink} className="activity-viewer__link-go-back">
         <Arrow className="activity-viewer__arrow-ico" />
       </Link>
-      <h1 className="activity-viewer__header">Тренировочный бой юниоров</h1>
-      <div className="activity-viewer__description">
-        <p>
-          Соревнование между совершеннолетними (18-25 лет) спортсменами с
-          базовым (начальным) уровнем подготовки с малым количеством (не более
-          3-ёх матчей, учитывая тренировочные) боевого опыта или полным его
-          отсутствием.
-        </p>
-
-        <p>
-          Десять команд по пятнадцать человек будут сражаться друг против друга
-          за первенство в нашем тренировочном бою. Победители получат сертификат
-          о прохождении боевой подготовки лазерного боя, а остальные спортсмены
-          - сертификат об участии.
-        </p>
-      </div>
+      <h1 className="activity-viewer__header">{header}</h1>
+      <div className="activity-viewer__description">{description}</div>
       <div className="activity-viewer__bottom-part">
-        <div className="activity-viewer__date">03.12</div>
-        <Link to="/activities" className="activity-viewer__link-send-request">
+        <div className="activity-viewer__date">{date}</div>
+        <Link
+          to={sendRequestLink}
+          className="activity-viewer__link-send-request"
+        >
           Подать заявку
         </Link>
       </div>
