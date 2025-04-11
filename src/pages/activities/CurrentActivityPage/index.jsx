1a1eccf493327acdff2cf0182b7c7c114f05c428
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-11 15:44:26 +0500

    remove unnecessary elements

--- src/pages/activities/CurrentActivityPage/index.jsx
+++ src/pages/activities/CurrentActivityPage/index.jsx
@@ -1,7 +1,3 @@
-import { Link } from "react-router-dom";
-
-// import { ReactComponent as Arrow } from "../../../svg/arrow.svg";
-
 import ActivityViewer from "../../../components/ActivityViewer";
 
 import "./index.css";
