57a12a259b2d272ea0b6ff6eccd3a54b8e2890c0
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-12 12:44:35 +0500

    add styles to cabinet activity page

--- src/pages/cabinet/cabinet-views/activities/CabinetActivityPage/index.jsx
+++ src/pages/cabinet/cabinet-views/activities/CabinetActivityPage/index.jsx
@@ -1,7 +1,26 @@
+import ActivityViewer from "../../../../../components/ActivityViewer";
+
 import "./index.css";
 
 const CabinetActivityPage = () => {
-  return "щас тут будет мероприятия";
+  return (
+    <div className="cabinet-activity-page">
+      <h1 className="cabinet-activity-page__header">Мероприятия</h1>
+      <ActivityViewer
+        goBackLink="/"
+        header="Тренировочный бой юниоров"
+        description="Соревнование между совершеннолетними (18-25 лет) спортсменами с
+        базовым (начальным) уровнем подготовки с малым количеством (не более
+        3-ёх матчей, учитывая тренировочные) боевого опыта или полным его
+        отсутствием. Десять команд по пятнадцать человек будут сражаться друг против друга
+        за первенство в нашем тренировочном бою. Победители получат сертификат
+        о прохождении боевой подготовки лазерного боя, а остальные спортсмены
+        - сертификат об участии."
+        date="03.12"
+        sendRequestLink="/"
+      />
+    </div>
+  );
 };
 
 export default CabinetActivityPage;
