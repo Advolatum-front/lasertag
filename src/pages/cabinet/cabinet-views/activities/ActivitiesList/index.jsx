04ae55f55f7324d3d841ab648f2eba64bed1d631
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-14 15:48:45 +0500

    almost done stylizing

--- src/pages/cabinet/cabinet-views/activities/ActivitiesList/index.jsx
+++ src/pages/cabinet/cabinet-views/activities/ActivitiesList/index.jsx
@@ -1,5 +1,3 @@
-// import {Link} from "react-router-dom";
-
 import LabeledInput from "../../../../../components/controls/LabeledInput";
 
 import "./index.css";
@@ -8,59 +6,58 @@ const ActivitiesList = () => {
   return (
     <div className="cabinet-activities">
       <h1 className="cabinet-activities__header">Мероприятия</h1>
-      <div className="cabinet-activities__filter-block">
-        <ul className="cabinet-activities__filter">
-          <li className="cabinet-activities__list-item">
-            <span className="cabinet-activities__current-option">Все</span>
-          </li>
-          <li className="cabinet-activities__list-item">
-            <button type="button" className="cabinet-activities__option">
-              Активные
-            </button>
-          </li>
-          <li className="cabinet-activities__list-item">
-            <button
-              type="button"
-              className="cabinet-activities__current-option"
-            >
-              Доступные
-            </button>
-          </li>
-        </ul>
-        <form className="cabinet-activities__seacrh-form">
-          <LabeledInput id="seacrhInput" label="Поиск по названию" />
-        </form>
-      </div>
-      <div className="cabinet-activities__content">
-        <ul className="cabinet-activities__captions-list">
-          <li className="cabinet-activities__caption-item date">Дата</li>
-          <li className="cabinet-activities__caption-item name">
-            Наименование
-          </li>
-          <li className="cabinet-activities__caption-item description">
-            Опсиание
-          </li>
-          <li className="cabinet-activities__caption-item status">
-            Статус заявки
-          </li>
-        </ul>
-        <ul className="cabinet-activities__activities-list">
-          <li className="cabinet-activities__activity-item">
-            <div className="cabinet-activities__activity-date">12.05</div>
-            <div className="cabinet-activities__activity-name">
-              Кубок мира по лазерному бою
-            </div>
-            <div className="cabinet-activities__activity-description">
-              Долгожданная борьба за мировое чемпионство по лазерному бою
-            </div>
-            <div className="cabinet-activities__activity-status approved">
-              Одобрена
-            </div>
-          </li>
-        </ul>
-        <div className="cabinet-activities__notification">
-          Список мероприятий постоянно пополняется. Не забывайте почаще навещать
-          нас.
+      <div className="cabinet-activities__page-content">
+        <div className="cabinet-activities__filter-block">
+          <ul className="cabinet-activities__filter">
+            <li className="cabinet-activities__list-item">
+              <span className="cabinet-activities__option current">Все</span>
+            </li>
+            <li className="cabinet-activities__list-item">
+              <button type="button" className="cabinet-activities__option">
+                Активные
+              </button>
+            </li>
+            <li className="cabinet-activities__list-item">
+              <button type="button" className="cabinet-activities__option">
+                Доступные
+              </button>
+            </li>
+          </ul>
+          <form className="cabinet-activities__seacrh-form">
+            <LabeledInput id="seacrhInput" label="Поиск по названию" />
+          </form>
+        </div>
+        <div className="cabinet-activities__content">
+          <ul className="cabinet-activities__captions-list">
+            <li className="cabinet-activities__caption-item date">Дата</li>
+            <li className="cabinet-activities__caption-item name">
+              Наименование
+            </li>
+            <li className="cabinet-activities__caption-item description">
+              Описание
+            </li>
+            <li className="cabinet-activities__caption-item status">
+              Статус заявки
+            </li>
+          </ul>
+          <ul className="cabinet-activities__activities-list">
+            <li className="cabinet-activities__activity-item">
+              <div className="cabinet-activities__activity-date">12.05</div>
+              <div className="cabinet-activities__activity-name">
+                Кубок мира по лазерному бою
+              </div>
+              <div className="cabinet-activities__activity-description">
+                Долгожданная борьба за мировое чемпионство по лазерному бою
+              </div>
+              <div className="cabinet-activities__activity-status approved">
+                Одобрена
+              </div>
+            </li>
+          </ul>
+          <div className="cabinet-activities__tip">
+            Список мероприятий постоянно пополняется. Не забывайте почаще
+            навещать нас.
+          </div>
         </div>
       </div>
     </div>
