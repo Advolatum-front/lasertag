dfb18485bf840c30047372dc3f54fe5419db1d71
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-15 19:58:03 +0500

    stylize media list

--- src/pages/cabinet/cabinet-views/favorites/CabinetGallery/index.jsx
+++ src/pages/cabinet/cabinet-views/favorites/CabinetGallery/index.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import { ReactComponent as Cross } from "../../../../../svg/cross-ico.svg";
+
 import "./index.css";
 
 const CabinetGallery = () => {
@@ -8,58 +10,124 @@ const CabinetGallery = () => {
       <h1 className="cabinet-gallery__header">Избранное</h1>
       <ul className="cabinet-gallery__media-list">
         <li className="cabinet-gallery__media-list-item">
-          <Link to="/" className="cabinet-gallery__link">
-            <img
-              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
-              alt=""
-              className="cabinet-gallery__photo"
-            />
-          </Link>
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <Link to="/" className="cabinet-gallery__link">
-            <img
-              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
-              alt=""
-              className="cabinet-gallery__photo"
-            />
-          </Link>
+          <video className="cabinet-gallery__element  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <Link to="/" className="cabinet-gallery__link">
-            <img
-              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
-              alt=""
-              className="cabinet-gallery__photo"
-            />
-          </Link>
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-2.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <Link to="/" className="cabinet-gallery__link">
-            <img
-              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
-              alt=""
-              className="cabinet-gallery__photo"
-            />
-          </Link>
+          <video className="cabinet-gallery__element  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <Link to="/" className="cabinet-gallery__link">
-            <img
-              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
-              alt=""
-              className="cabinet-gallery__photo"
-            />
-          </Link>
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-3.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <Link to="/" className="cabinet-gallery__link">
-            <img
-              src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
-              alt=""
-              className="cabinet-gallery__photo"
-            />
-          </Link>
+          <video className="cabinet-gallery__element  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-3.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
         </li>
       </ul>
     </div>
