b36a0a8e39af986961f2c060ec6884f547577159
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-15 20:17:16 +0500

    fix heigt bug

--- src/pages/cabinet/cabinet-views/favorites/CabinetGallery/index.jsx
+++ src/pages/cabinet/cabinet-views/favorites/CabinetGallery/index.jsx
@@ -21,17 +21,8 @@ const CabinetGallery = () => {
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <video className="cabinet-gallery__element  ">
-            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
-          </video>
-          <Link to="/" className="cabinet-gallery__link" />
-          <button className="cabinet-gallery__button-unlike">
-            <Cross className="cabinet-gallery__cross-ico" />
-          </button>
-        </li>
-        <li className="cabinet-gallery__media-list-item">
           <img
-            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-2.webp"
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
             alt=""
             className="cabinet-gallery__element"
           />
@@ -41,7 +32,7 @@ const CabinetGallery = () => {
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <video className="cabinet-gallery__element  ">
+          <video className="cabinet-gallery__element">
             <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
           </video>
           <Link to="/" className="cabinet-gallery__link" />
@@ -50,18 +41,25 @@ const CabinetGallery = () => {
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <img
-            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-3.webp"
-            alt=""
-            className="cabinet-gallery__element"
-          />
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
           <Link to="/" className="cabinet-gallery__link" />
           <button className="cabinet-gallery__button-unlike">
             <Cross className="cabinet-gallery__cross-ico" />
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <video className="cabinet-gallery__element  ">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
             <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
           </video>
           <Link to="/" className="cabinet-gallery__link" />
@@ -81,17 +79,8 @@ const CabinetGallery = () => {
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <video className="cabinet-gallery__element  ">
-            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
-          </video>
-          <Link to="/" className="cabinet-gallery__link" />
-          <button className="cabinet-gallery__button-unlike">
-            <Cross className="cabinet-gallery__cross-ico" />
-          </button>
-        </li>
-        <li className="cabinet-gallery__media-list-item">
           <img
-            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-3.webp"
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
             alt=""
             className="cabinet-gallery__element"
           />
@@ -101,7 +90,34 @@ const CabinetGallery = () => {
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <video className="cabinet-gallery__element  ">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
             <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
           </video>
           <Link to="/" className="cabinet-gallery__link" />
@@ -121,7 +137,45 @@ const CabinetGallery = () => {
           </button>
         </li>
         <li className="cabinet-gallery__media-list-item">
-          <video className="cabinet-gallery__element  ">
+          <img
+            src="/img/gallery/photo/albums/1/thumbnails/thumbnail-1.webp"
+            alt=""
+            className="cabinet-gallery__element"
+          />
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <Link to="/" className="cabinet-gallery__link" />
+          <button className="cabinet-gallery__button-unlike">
+            <Cross className="cabinet-gallery__cross-ico" />
+          </button>
+        </li>
+        <li className="cabinet-gallery__media-list-item">
+          <video className="cabinet-gallery__element">
             <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
           </video>
           <Link to="/" className="cabinet-gallery__link" />
