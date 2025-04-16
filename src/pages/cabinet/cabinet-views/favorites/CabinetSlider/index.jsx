efe909c8e2194279c7635a3be132dcfa3e44d45c
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-17 00:55:44 +0500

    set slider width, stylyze header

--- src/pages/cabinet/cabinet-views/favorites/CabinetSlider/index.jsx
+++ src/pages/cabinet/cabinet-views/favorites/CabinetSlider/index.jsx
@@ -1,7 +1,76 @@
+// import { Link } from "react-router-dom";
+
+import { ReactComponent as Cross } from "../../../../../svg/cross-ico.svg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
 import "./index.css";
 
 const CabinetSlider = () => {
-  return "Слайдер кабинета";
+  return (
+    <div className="cabinet-slider">
+      <h1 className="cabinet-slider__header">Избранное</h1>
+      <Swiper
+        autoHeight={false}
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        navigation={true}
+        modules={[Navigation]}
+        className="cabinet-slider"
+      >
+        <SwiperSlide className="cabinet-slider__slide">
+          <img
+            src="/img/gallery/photo/albums/1/big/photo-1.jpg"
+            alt=""
+            className="cabinet-slider__image-bg"
+          />
+          <img
+            src="/img/gallery/photo/albums/1/big/photo-1.jpg"
+            alt=""
+            className="cabinet-slider__picture"
+          />
+          <div className="cabinet-slider__panel">
+            <button className="cabinet-slider__button-close">
+              <Cross className="cross" />
+            </button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="cabinet-slider__slide">
+          <img
+            src="/img/gallery/photo/albums/1/big/photo-2.jpg"
+            alt=""
+            className="cabinet-slider__image-bg"
+          />
+          <img
+            src="/img/gallery/photo/albums/1/big/photo-2.jpg"
+            alt=""
+            className="cabinet-slider__picture"
+          />
+          <div className="cabinet-slider__panel">
+            <button className="cabinet-slider__button-close">
+              <Cross className="cross" />
+            </button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="cabinet-slider__slide">
+          <div className="cabinet-slider__video-bg"></div>
+          <video controls className="cabinet-slider__picture  ">
+            <source src="/img/gallery/video/1/radio.mp4" type="video/mp4" />
+          </video>
+          <div className="cabinet-slider__panel">
+            <button className="cabinet-slider__button-close">
+              <Cross className="cross" />
+            </button>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default CabinetSlider;
