2fc9162ce45bd368e1584f486d2e4ecd18094a5a
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-17 23:19:51 +0500

    add closest activity list layout

--- src/pages/activities/ActivitiesCalendar/index.jsx
+++ src/pages/activities/ActivitiesCalendar/index.jsx
@@ -1,7 +1,63 @@
+import { Link } from "react-router-dom";
+
 import "./index.css";
 
 const ActivitiesCalendar = () => {
-  return "Календарь";
+  return (
+    <section className="calendar-section">
+      <h1 className="calendar-section__header">Мероприятия</h1>
+      <h2 className="calendar-section__closest-activities-header">
+        Ближайшие мероприятия
+      </h2>
+      <ul className="calendar-section__closest-activities-list">
+        <li className="calendar-section__closest-activity-item">
+          <div className="calendar-section__activity-date">03.12</div>
+          <div className="calendar-section__activity-info">
+            <div className="calendar-section__activity-top-part">
+              <h3 className="calendar-section__activity-header">
+                Тренировочный бой юниоров
+              </h3>
+              <Link className="calendar-section__link-more">Подробнее</Link>
+            </div>
+            <div className="calendar-section__description">
+              Подробное описание мероприятия для полноты картины, но не совсем,
+              чтобы можно было перейти по кнопке и прочитать больше информации
+            </div>
+          </div>
+        </li>
+        <li className="calendar-section__closest-activity-item">
+          <div className="calendar-section__activity-date">03.12</div>
+          <div className="calendar-section__activity-info">
+            <div className="calendar-section__activity-top-part">
+              <h3 className="calendar-section__activity-header">
+                Тренировочный бой юниоров
+              </h3>
+              <Link className="calendar-section__link-more">Подробнее</Link>
+            </div>
+            <div className="calendar-section__description">
+              Подробное описание мероприятия для полноты картины, но не совсем,
+              чтобы можно было перейти по кнопке и прочитать больше информации
+            </div>
+          </div>
+        </li>
+        <li className="calendar-section__closest-activity-item">
+          <div className="calendar-section__activity-date">03.12</div>
+          <div className="calendar-section__activity-info">
+            <div className="calendar-section__activity-top-part">
+              <h3 className="calendar-section__activity-header">
+                Тренировочный бой юниоров
+              </h3>
+              <Link className="calendar-section__link-more">Подробнее</Link>
+            </div>
+            <div className="calendar-section__description">
+              Подробное описание мероприятия для полноты картины, но не совсем,
+              чтобы можно было перейти по кнопке и прочитать больше информации
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default ActivitiesCalendar;
