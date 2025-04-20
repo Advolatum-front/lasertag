878fa500b3c62056788a7e98608ff60ef6e770f3
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-20 18:31:38 +0500

    add basic layout for calendar

--- src/components/ActivitiesCalendar/index.jsx
+++ src/components/ActivitiesCalendar/index.jsx
@@ -1,7 +1,73 @@
+import { Link } from "react-router-dom";
+
 import "./index.css";
 
 const ActivitiesCalendar = () => {
-  return "Это календарь мероприятий";
+  const daysOfWeekCaptions = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+  const daysOfWeekItems = daysOfWeekCaptions.map((item, index) => {
+    return (
+      <li className="activities-calendar__day-caption" key={index}>
+        {item}
+      </li>
+    );
+  });
+
+  return (
+    <div className="activities-calendar-wrapper">
+      <ul className="activities-calendar__days-of-week">{daysOfWeekItems}</ul>
+      <ul className="activities-calendar__calendar">
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+        <li className="activities-calendar__calendar-day">
+          <span className="activities-calendar__day-number">01</span>
+          <span className="activities-calendar__activity-name">
+            Тренировочный бой юниоров
+          </span>
+          <Link to="/" className="activities-calendar-__link" />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default ActivitiesCalendar;
