4981aacff0b9bc6c006647365a7e851390ec0af2
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-23 14:43:51 +0500

    insert video

--- src/components/index-page/Hero/index.jsx
+++ src/components/index-page/Hero/index.jsx
@@ -14,13 +14,19 @@ const Hero = () => {
     setPopupOpened(false);
   };
 
+  const video = (
+    <video controls>
+      <source src="/index-page/promo.mp4" type="video/mp4" />
+    </video>
+  );
+
   return (
     <section className="hero">
       <button className="hero__button-watch-promo" onClick={showPopup}>
         Смотреть проморолик
       </button>
       <PopupChildren open={popupOpened} onClose={closePopup}>
-        <p>Привет</p>
+        {video}
       </PopupChildren>
     </section>
   );
