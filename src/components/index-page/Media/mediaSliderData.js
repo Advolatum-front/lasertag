b44c32a502821ca669418d29531aa30b8b656202
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-23 19:33:24 +0500

    change photos for index slider

--- src/components/index-page/Media/mediaSliderData.js
+++ src/components/index-page/Media/mediaSliderData.js
@@ -3,4 +3,8 @@ export const mediaSliderData = [
   "/gallery/index-slider/2.webp",
   "/gallery/index-slider/3.webp",
   "/gallery/index-slider/4.webp",
+  "/gallery/index-slider/5.webp",
+  "/gallery/index-slider/6.webp",
+  "/gallery/index-slider/7.webp",
+  "/gallery/index-slider/8.webp",
 ];
