1bef6a19a8ac77f9453343b4e881f722b0ebbace
Advolatum-front <Advolatum-front@yandex.ru>
2025-04-29 03:09:18 +0500

    add methods to let last news

--- src/stores/newsStore.js
+++ src/stores/newsStore.js
@@ -6,16 +6,19 @@ class NewsStore {
   newsList = [];
   fetchedNewsItem = null;
   adjacentNewsIds = [];
+  lastNews = [];
 
   constructor() {
     makeObservable(this, {
       newsList: observable,
       fetchedNewsItem: observable,
       adjacentNewsIds: observable,
+      lastNews: observable,
 
       fetchNews: action,
       fetchNewsItemById: action,
       fetchAdjacentNewsIdsById: action,
+      fetchLastNews: action,
     });
   }
 
@@ -34,6 +37,20 @@ class NewsStore {
     this.adjacentNewsIds = getAdjacentByIndex(this.newsList, index).map(
       (item) => item.id,
     );
+
+    fetchLastNews = (count) => {
+      this.fetchNews();
+
+      this.lastNews = this.newsList
+        .toSorted((a, b) => {
+          const [dateNumA, dateNumB] = [a.date, b.date].map((date) => {
+            return Number(date.replace(".", ""));
+          });
+
+          return dateNumB - dateNumA;
+        })
+        .slice(-count);
+    };
   };
 }
 
