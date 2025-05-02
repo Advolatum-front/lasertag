45f36fc7b2dd6a704dbe52205f4c62c1906f6693
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-02 23:04:01 +0500

    inject AcitivityStore in activities section

--- src/components/index-page/Activities/index.jsx
+++ src/components/index-page/Activities/index.jsx
@@ -1,58 +1,64 @@
 import { Link } from "react-router-dom";
+import { inject, observer } from "mobx-react";
 
 import "./index.css";
 
-const Activities = () => {
-  return (
-    <section className="activities">
-      <div className="activities__shooter"></div>
-      <h2 className="activities__section-header">Мероприятия</h2>
-      <ul className="activities__list">
-        <li className="activities__list-item">
-          <div className="activities__date">11.06</div>
-          <div className="activities__info">
-            <div className="activities__header">Наименование мероприятия</div>
-            <div className="activities__description">
-              Подробное описание мероприятия для полноты картины, но не совсем,
-              чтобы можно было перейти по кнопке и прочитать больше информации
+const Activities = inject("ActivitiesStore")(
+  observer(({ ActivitiesStore }) => {
+    return (
+      <section className="activities">
+        <div className="activities__shooter"></div>
+        <h2 className="activities__section-header">Мероприятия</h2>
+        <ul className="activities__list">
+          <li className="activities__list-item">
+            <div className="activities__date">11.06</div>
+            <div className="activities__info">
+              <div className="activities__header">Наименование мероприятия</div>
+              <div className="activities__description">
+                Подробное описание мероприятия для полноты картины, но не
+                совсем, чтобы можно было перейти по кнопке и прочитать больше
+                информации
+              </div>
+              <Link to="/activities/id" className="activities__link">
+                Подробнее
+              </Link>
             </div>
-            <Link to="/activities/id" className="activities__link">
-              Подробнее
-            </Link>
-          </div>
-        </li>
-        <li className="activities__list-item">
-          <div className="activities__date">11.06</div>
-          <div className="activities__info">
-            <div className="activities__header">Наименование мероприятия</div>
-            <div className="activities__description">
-              Подробное описание мероприятия для полноты картины, но не совсем,
-              чтобы можно было перейти по кнопке и прочитать больше информации
+          </li>
+          <li className="activities__list-item">
+            <div className="activities__date">11.06</div>
+            <div className="activities__info">
+              <div className="activities__header">Наименование мероприятия</div>
+              <div className="activities__description">
+                Подробное описание мероприятия для полноты картины, но не
+                совсем, чтобы можно было перейти по кнопке и прочитать больше
+                информации
+              </div>
+              <Link to="/activities/id" className="activities__link">
+                Подробнее
+              </Link>
             </div>
-            <Link to="/activities/id" className="activities__link">
-              Подробнее
-            </Link>
-          </div>
-        </li>
-        <li className="activities__list-item">
-          <div className="activities__date">11.06</div>
-          <div className="activities__info">
-            <div className="activities__header">Наименование мероприятия</div>
-            <div className="activities__description">
-              Подробное описание мероприятия для полноты картины, но не совсем,
-              чтобы можно было перейти по кнопке и прочитать больше информации
+          </li>
+          <li className="activities__list-item">
+            <div className="activities__date">11.06</div>
+            <div className="activities__info">
+              <div className="activities__header">Наименование мероприятия</div>
+              <div className="activities__description">
+                Подробное описание мероприятия для полноты картины, но не
+                совсем, чтобы можно было перейти по кнопке и прочитать больше
+                информации
+              </div>
+              <Link to="/activities/id" className="activities__link">
+                Подробнее
+              </Link>
             </div>
-            <Link to="/activities/id" className="activities__link">
-              Подробнее
-            </Link>
-          </div>
-        </li>
-      </ul>
-      <Link to="/" className="activities__link-to-all">
-        <span>Все мероприятия</span>
-      </Link>
-    </section>
-  );
-};
+          </li>
+        </ul>
+        <Link to="/" className="activities__link-to-all">
+          <span>Все мероприятия</span>
+        </Link>
+      </section>
+    );
+  }),
+);
 
 export default Activities;
