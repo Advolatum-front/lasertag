42dc9c472f019d213d793b406ab524f8c2ad73f8
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-03 01:45:24 +0500

    fetch upcoming activities on index page

--- src/components/index-page/Activities/index.jsx
+++ src/components/index-page/Activities/index.jsx
@@ -1,58 +1,58 @@
 import { Link } from "react-router-dom";
 import { inject, observer } from "mobx-react";
+import { useEffect } from "react";
+
+import NoData from "../../../components/NoData";
+
+import { stripYear } from "../../../utils/date/functions";
 
 import "./index.css";
 
 const Activities = inject("ActivitiesStore")(
   observer(({ ActivitiesStore }) => {
+    const { fetchUpcomingActivities, upcomingActivities } = ActivitiesStore;
+
+    useEffect(() => {
+      fetchUpcomingActivities();
+    }, []);
+
+    if (upcomingActivities.length === 0) {
+      return (
+        <section className="activities">
+          <div className="activities__shooter" />
+          <h2 className="activities__section-header">Мероприятия</h2>
+          <NoData />
+          <Link to="/activities" className="activities__link-to-all">
+            <span>Все мероприятия</span>
+          </Link>
+        </section>
+      );
+    }
+
+    const activityItems = upcomingActivities.map((item) => {
+      const { id, date, title, description } = item;
+      const visibleDate = stripYear(date);
+      const url = `/activities/${id}`;
+
+      return (
+        <li className="activities__list-item">
+          <div className="activities__date">{visibleDate}</div>
+          <div className="activities__info">
+            <div className="activities__header">{title}</div>
+            <div className="activities__description">{description}</div>
+            <Link to={url} className="activities__link">
+              Подробнее
+            </Link>
+          </div>
+        </li>
+      );
+    });
+
     return (
       <section className="activities">
-        <div className="activities__shooter"></div>
+        <div className="activities__shooter" />
         <h2 className="activities__section-header">Мероприятия</h2>
-        <ul className="activities__list">
-          <li className="activities__list-item">
-            <div className="activities__date">11.06</div>
-            <div className="activities__info">
-              <div className="activities__header">Наименование мероприятия</div>
-              <div className="activities__description">
-                Подробное описание мероприятия для полноты картины, но не
-                совсем, чтобы можно было перейти по кнопке и прочитать больше
-                информации
-              </div>
-              <Link to="/activities/id" className="activities__link">
-                Подробнее
-              </Link>
-            </div>
-          </li>
-          <li className="activities__list-item">
-            <div className="activities__date">11.06</div>
-            <div className="activities__info">
-              <div className="activities__header">Наименование мероприятия</div>
-              <div className="activities__description">
-                Подробное описание мероприятия для полноты картины, но не
-                совсем, чтобы можно было перейти по кнопке и прочитать больше
-                информации
-              </div>
-              <Link to="/activities/id" className="activities__link">
-                Подробнее
-              </Link>
-            </div>
-          </li>
-          <li className="activities__list-item">
-            <div className="activities__date">11.06</div>
-            <div className="activities__info">
-              <div className="activities__header">Наименование мероприятия</div>
-              <div className="activities__description">
-                Подробное описание мероприятия для полноты картины, но не
-                совсем, чтобы можно было перейти по кнопке и прочитать больше
-                информации
-              </div>
-              <Link to="/activities/id" className="activities__link">
-                Подробнее
-              </Link>
-            </div>
-          </li>
-        </ul>
+        <ul className="activities__list">{activityItems}</ul>
         <Link to="/activities" className="activities__link-to-all">
           <span>Все мероприятия</span>
         </Link>
