1252c30423ebca42053e8eb9b83ef4bab69809c5
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-03 00:55:02 +0500

    change date in newsStore

--- src/utils/date/functions.js
+++ src/utils/date/functions.js
@@ -1,10 +1,3 @@
-import { dateTimeParse } from "@gravity-ui/date-utils";
-const DEFAULT_FORMAT = "DD.MM";
-
-export const formatDate = (date, format = DEFAULT_FORMAT) => {
-  return dateTimeParse(date).format(format);
-};
-
 export const stripYear = (DDMMYYYDate) => {
   const [d, m, _] = DDMMYYYDate.split(".");
   return `${d}.${m}`;
