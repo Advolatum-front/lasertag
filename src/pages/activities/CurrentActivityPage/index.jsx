9a625f8c435a6fbdff190045ddc55d703725f16e
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-03 14:43:26 +0500

    adjust activityViewer dislay depeding on activity By Id exists

--- src/pages/activities/CurrentActivityPage/index.jsx
+++ src/pages/activities/CurrentActivityPage/index.jsx
@@ -1,29 +1,47 @@
 import { useParams } from "react-router";
+import { useEffect } from "react";
+import { inject, observer } from "mobx-react";
 
 import ActivityViewer from "../../../components/ActivityViewer";
+import NoData from "../../../components/NoData";
 
 import "./index.css";
 
-const CurrentActivityPage = () => {
-  console.log("param: ", useParams().id);
+const CurrentActivityPage = inject("ActivitiesStore")(
+  observer(({ ActivitiesStore }) => {
+    const id = useParams().id;
+    const { fetchedActivityItem, fetchActivityItemById } = ActivitiesStore;
 
-  return (
-    <section className="current-activity">
-      <ActivityViewer
-        goBackLink="/"
-        header="Тренировочный бой юниоров"
-        description="Соревнование между совершеннолетними (18-25 лет) спортсменами с
-          базовым (начальным) уровнем подготовки с малым количеством (не более
-          3-ёх матчей, учитывая тренировочные) боевого опыта или полным его
-          отсутствием. Десять команд по пятнадцать человек будут сражаться друг против друга
-          за первенство в нашем тренировочном бою. Победители получат сертификат
-          о прохождении боевой подготовки лазерного боя, а остальные спортсмены
-          - сертификат об участии."
-        date="03.12"
-        sendRequestLink="/"
-      />
-    </section>
-  );
-};
+    useEffect(() => {
+      fetchActivityItemById(id);
+    }, [fetchActivityItemById, id]);
+
+    if (!fetchedActivityItem) {
+      return (
+        <section className="current-activity">
+          <NoData />
+        </section>
+      );
+    }
+
+    return (
+      <section className="current-activity">
+        <ActivityViewer
+          goBackLink="/"
+          header="Тренировочный бой юниоров"
+          description="Соревнование между совершеннолетними (18-25 лет) спортсменами с
+            базовым (начальным) уровнем подготовки с малым количеством (не более
+            3-ёх матчей, учитывая тренировочные) боевого опыта или полным его
+            отсутствием. Десять команд по пятнадцать человек будут сражаться друг против друга
+            за первенство в нашем тренировочном бою. Победители получат сертификат
+            о прохождении боевой подготовки лазерного боя, а остальные спортсмены
+            - сертификат об участии."
+          date="03.12"
+          sendRequestLink="/"
+        />
+      </section>
+    );
+  }),
+);
 
 export default CurrentActivityPage;
