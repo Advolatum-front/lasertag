1bd3fd35e010fc8b8523cbaa5a839ab1d7cbdf1e
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-04 22:03:17 +0500

    inject usersstore in cabinet page

--- src/pages/cabinet/IndexCabinet/index.jsx
+++ src/pages/cabinet/IndexCabinet/index.jsx
@@ -1,4 +1,10 @@
-import { Link, useLocation, Outlet } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  Outlet,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
 
 import { useState } from "react";
 
@@ -6,118 +12,128 @@ import { ReactComponent as Cross } from "../../../svg/cross-ico.svg";
 
 import Confirm from "../../../components/popups/Confirm";
 
+import { inject, observer } from "mobx-react";
+
 import "./index.css";
 
-const IndexCabinet = () => {
-  const locationPathname = useLocation().pathname;
-  const [cabinetMenuOpened, setCabinetMenuOpened] = useState(false);
-  const [confirmOpen, setConfirmOpen] = useState(false);
+const IndexCabinet = inject("UsersStore")(
+  observer(({ UsersStore }) => {
+    const { logoutUser } = UsersStore;
+    const navigate = useNavigate();
 
-  const openCabinetMenu = () => {
-    setCabinetMenuOpened(true);
-  };
+    const locationPathname = useLocation().pathname;
+    const [cabinetMenuOpened, setCabinetMenuOpened] = useState(false);
+    const [confirmOpen, setConfirmOpen] = useState(false);
 
-  const closeCabinetMenu = () => {
-    setCabinetMenuOpened(false);
-  };
+    const openCabinetMenu = () => {
+      setCabinetMenuOpened(true);
+    };
 
-  const openConfirm = () => {
-    setConfirmOpen(true);
-  };
+    const closeCabinetMenu = () => {
+      setCabinetMenuOpened(false);
+    };
 
-  const closeConfirm = () => {
-    setConfirmOpen(false);
-  };
+    const openConfirm = () => {
+      setConfirmOpen(true);
+    };
 
-  const comfirmLogout = () => {
-    setConfirmOpen(false);
-    setCabinetMenuOpened(false);
-  };
+    const closeConfirm = () => {
+      setConfirmOpen(false);
+    };
 
-  const cabinetMenuClassName = cabinetMenuOpened
-    ? "cabinet-menu opened"
-    : "cabinet-menu";
+    const comfirmLogout = () => {
+      setConfirmOpen(false);
+      setCabinetMenuOpened(false);
 
-  const menuDataLinks = [
-    {
-      caption: "Мероприятия",
-      link: "/cabinet/activities",
-    },
-    {
-      caption: "Избранное",
-      link: "/cabinet/favorites",
-    },
-    {
-      caption: "Мой профиль",
-      link: "/cabinet/myprofile",
-    },
-  ];
+      logoutUser();
+      navigate("/");
+    };
 
-  const menuLinkItems = menuDataLinks.map((item, index) => {
-    const { caption, link } = item;
-    const linkClassName = locationPathname.includes(link)
-      ? "cabinet-menu__link-active"
-      : "cabinet-menu__link";
+    const cabinetMenuClassName = cabinetMenuOpened
+      ? "cabinet-menu opened"
+      : "cabinet-menu";
+
+    const menuDataLinks = [
+      {
+        caption: "Мероприятия",
+        link: "/cabinet/activities",
+      },
+      {
+        caption: "Избранное",
+        link: "/cabinet/favorites",
+      },
+      {
+        caption: "Мой профиль",
+        link: "/cabinet/myprofile",
+      },
+    ];
+
+    const menuLinkItems = menuDataLinks.map((item, index) => {
+      const { caption, link } = item;
+      const linkClassName = locationPathname.includes(link)
+        ? "cabinet-menu__link-active"
+        : "cabinet-menu__link";
+
+      return (
+        <li className="cabinet-menu__list-item" key={index}>
+          <Link to={link} className={linkClassName} onClick={closeCabinetMenu}>
+            {caption}
+          </Link>
+        </li>
+      );
+    });
 
     return (
-      <li className="cabinet-menu__list-item" key={index}>
-        <Link to={link} className={linkClassName} onClick={closeCabinetMenu}>
-          {caption}
-        </Link>
-      </li>
-    );
-  });
-
-  return (
-    <div className="index-cabinet">
-      <div className="index-cabinet__bugrer-button-menu-container">
-        <button
-          className="index-cabinet__bugrer-button"
-          onClick={openCabinetMenu}
-        >
-          <i></i>
-          <i></i>
-          <i></i>
-        </button>
-      </div>
-      <aside className={cabinetMenuClassName}>
-        <button
-          className="cabinet-menu__button-close"
-          onClick={closeCabinetMenu}
-        >
-          <Cross />
-        </button>
-        <div className="cabinet-menu__user-card">
-          <img
-            src="/userpics/userpic-1.png"
-            alt=""
-            className="cabinet-menu__user-pic"
-          />
-          <div className="cabinet-menu__user-name">Мария Богуш</div>
-          <div className="cabinet-menu__user-id">id: 598613245896</div>
+      <div className="index-cabinet">
+        <div className="index-cabinet__bugrer-button-menu-container">
+          <button
+            className="index-cabinet__bugrer-button"
+            onClick={openCabinetMenu}
+          >
+            <i></i>
+            <i></i>
+            <i></i>
+          </button>
         </div>
-        <ul className="cabinet-menu__nav-list">
-          {menuLinkItems}
-          <li className="cabinet-menu__list-item">
-            <button
-              className="cabinet-menu__button-logout"
-              onClick={openConfirm}
-            >
-              Выход
-            </button>
-          </li>
-        </ul>
-      </aside>
-      <Confirm
-        text="Вы уверены, что хотите выйти?"
-        open={confirmOpen}
-        onYes={comfirmLogout}
-        onNo={closeConfirm}
-        onClose={closeConfirm}
-      />
-      <Outlet />
-    </div>
-  );
-};
+        <aside className={cabinetMenuClassName}>
+          <button
+            className="cabinet-menu__button-close"
+            onClick={closeCabinetMenu}
+          >
+            <Cross />
+          </button>
+          <div className="cabinet-menu__user-card">
+            <img
+              src="/userpics/userpic-1.png"
+              alt=""
+              className="cabinet-menu__user-pic"
+            />
+            <div className="cabinet-menu__user-name">Мария Богуш</div>
+            <div className="cabinet-menu__user-id">id: 598613245896</div>
+          </div>
+          <ul className="cabinet-menu__nav-list">
+            {menuLinkItems}
+            <li className="cabinet-menu__list-item">
+              <button
+                className="cabinet-menu__button-logout"
+                onClick={openConfirm}
+              >
+                Выход
+              </button>
+            </li>
+          </ul>
+        </aside>
+        <Confirm
+          text="Вы уверены, что хотите выйти?"
+          open={confirmOpen}
+          onYes={comfirmLogout}
+          onNo={closeConfirm}
+          onClose={closeConfirm}
+        />
+        <Outlet />
+      </div>
+    );
+  }),
+);
 
 export default IndexCabinet;
