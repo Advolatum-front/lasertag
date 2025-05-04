7370a4432680a943d9013e20c8b6ac5d8f1120f8
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-04 15:00:03 +0500

    add users store

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,11 @@
 import NewsStore from "./newsStore";
 import ActivitiesStore from "./activitiesStore";
+import UsersStore from "./UsersStore";
 
 const stores = {
   NewsStore,
   ActivitiesStore,
+  UsersStore,
 };
 
 export default stores;
---
+++ src/stores/usersStore.js
@@ -0,0 +1,6 @@
+import { makeObservable, action, observable, computed } from "mobx";
+
+class UsersStore {}
+
+const usersStore = new UsersStore();
+export default usersStore;
