185789c3d84730d50a8c5c3d88e085438ba62e37
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-05 20:34:43 +0500

    unject users store

--- src/pages/RestorePassword/index.jsx
+++ src/pages/RestorePassword/index.jsx
@@ -1,42 +1,47 @@
+import { inject, observer } from "mobx-react";
 import { Link } from "react-router-dom";
 
 import LabeledInput from "../../components/controls/LabeledInput";
 
+import MessageBlock from "../../components/MessageBlock";
+
 import "./index.css";
 
-const RestorePassword = () => {
-  const submitForm = (event) => {
-    event.preventDefault();
-  };
+const RestorePassword = inject("UsersStore")(
+  observer(({ UsersStore }) => {
+    const submitForm = (event) => {
+      event.preventDefault();
+    };
 
-  return (
-    <div className="restore-wrapper">
-      <form className="restore-wrapper__form" onSubmit={submitForm}>
-        <div className="restore-wrapper__form-header">
-          Восстановление пароля
-        </div>
-        <div className="restore-wrapper__form-tip">
-          Введите адрес электронной почты, связанный с вашим аккаунтом, а также
-          новый пароль и его подтверждение
-        </div>
-        <LabeledInput type="email" label="E-mail" required />
-        <LabeledInput type="email" id="email" label="Новый пароль" required />
-        <LabeledInput
-          type="email"
-          id="email"
-          label="Подтвердите новый пароль"
-          required
-        />
-        <button type="submit" className="restore-wrapper__submit">
-          Сбросить пароль
-        </button>
-        <Link to="/login" className="restore-wrapper__link">
-          Я вспомнил(а) пароль
-        </Link>
-      </form>
-      <div className="restore-wrapper__bg"></div>
-    </div>
-  );
-};
+    return (
+      <div className="restore-wrapper">
+        <form className="restore-wrapper__form" onSubmit={submitForm}>
+          <div className="restore-wrapper__form-header">
+            Восстановление пароля
+          </div>
+          <div className="restore-wrapper__form-tip">
+            Введите адрес электронной почты, связанный с вашим аккаунтом, а
+            также новый пароль и его подтверждение
+          </div>
+          <LabeledInput type="email" label="E-mail" required />
+          <LabeledInput type="email" id="email" label="Новый пароль" required />
+          <LabeledInput
+            type="email"
+            id="email"
+            label="Подтвердите новый пароль"
+            required
+          />
+          <button type="submit" className="restore-wrapper__submit">
+            Сбросить пароль
+          </button>
+          <Link to="/login" className="restore-wrapper__link">
+            Я вспомнил(а) пароль
+          </Link>
+        </form>
+        <div className="restore-wrapper__bg"></div>
+      </div>
+    );
+  }),
+);
 
 export default RestorePassword;
