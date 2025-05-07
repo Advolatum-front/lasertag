62e80707f21280fe1382762b343c1e5c137b30b1
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-08 03:02:42 +0500

    add form footer functionality

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { mainNavLinks } from "../../utils/main-nav-links.js";
+import MessageDlg from "../popups/MessageDlg/";
+
+import { mainNavLinks } from "../../utils/main-nav-links";
+
+import { PDT_ALERT } from "../../utils/popup-dialog-types";
 
 import logo from "../../img/logo.webp";
 
 import "./index.css";
 
 const Footer = () => {
+  const [alertOpen, setAlertOpen] = useState(false);
+
   const navListItemArray = mainNavLinks.map((obj, index) => {
     return (
       <li key={index}>
@@ -17,13 +24,22 @@ const Footer = () => {
     );
   });
 
+  const submitForm = (event) => {
+    event.preventDefault();
+    setAlertOpen(true);
+  };
+
+  const closeAlert = () => {
+    setAlertOpen(false);
+  };
+
   return (
     <footer className="footer">
       <div className="footer__logo-and-menu">
         <img src={logo} alt="" className="footer__logo" />
         <ul className="footer__menu">{navListItemArray}</ul>
       </div>
-      <form className="footer__feedback-form">
+      <form className="footer__feedback-form" onSubmit={submitForm}>
         <div className="footer__feedback-form-header">
           Связаться с нами можно здесь!
         </div>
@@ -39,9 +55,15 @@ const Footer = () => {
           placeholder="Ваш e-mail"
           required
         />
-        <button type="button" className="footer__submit-form">
+        <button type="submit" className="footer__submit-form">
           <span>Отправить</span>
         </button>
+        <MessageDlg
+          text="Мы с Вами свяжемся."
+          type={PDT_ALERT}
+          onClose={closeAlert}
+          open={alertOpen}
+        />
       </form>
     </footer>
   );
