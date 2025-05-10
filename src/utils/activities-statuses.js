b886df61cb26da8100e3950526bc4bcfd0442773
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-11 00:33:57 +0500

    add ativities requets status consts

--- src/utils/activities-statuses.js
+++ src/utils/activities-statuses.js
@@ -1,3 +1,11 @@
+// ARS = Activity Request Status
+
+export const ARS_NO_STATUS = 0;
+export const ARS_APPROVED = 1;
+export const ARS_DECLINED = 2;
+export const ARS_PENDING = 3;
+export const ARS_AVAIBLE = 4;
+
 export const ACTIVITIY_REQUEST_STATUSES = [
   { caption: "Нет статуса", className: "no-status" },
   { caption: "Одобрена", className: "approved" },
