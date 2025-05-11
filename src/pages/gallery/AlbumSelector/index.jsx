f193d3d4aafe87160ea325403f51894551e1e7eb
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-11 20:12:57 +0500

    fetch albums in album selector page

--- src/pages/gallery/AlbumSelector/index.jsx
+++ src/pages/gallery/AlbumSelector/index.jsx
@@ -33,42 +33,28 @@ const AlbumSelector = inject("GalleryStore")(
       );
     }
 
+    const albumListItems = albumsList.map((album, index) => {
+      const { id, title, cover } = album;
+      const urlLink = `/gallery/album/${id}`;
+      const albumLinkClass = `albums-list__link link-${index + 1}`;
+
+      return (
+        <li className="albums-list__item" key={id}>
+          <Link to={urlLink} className="albums-list__cover-container">
+            <img src={cover} alt="" className="albums-list__cover" />
+          </Link>
+          <Link to={urlLink} className={albumLinkClass}>
+            {title}
+          </Link>
+        </li>
+      );
+    });
+
     return (
       <>
         <GalleryNavigator className="gallery-navigator-mb" />
         <div className="albums-list-wrapper">
-          <ul className="albums-list">
-            <li className="albums-list__item">
-              <Link className="albums-list__cover-container">
-                <img
-                  src="/img/gallery/album-selector/photo/cover-1.webp"
-                  alt=""
-                  className="albums-list__cover"
-                />
-              </Link>
-              <Link className="albums-list__link link-1">Фото с региона</Link>
-            </li>
-            <li className="albums-list__item">
-              <Link className="albums-list__cover-container">
-                <img
-                  src="/img/gallery/album-selector/photo/cover-2.webp"
-                  alt=""
-                  className="albums-list__cover"
-                />
-              </Link>
-              <Link className="albums-list__link link-2">Фото с области</Link>
-            </li>
-            <li className="albums-list__item">
-              <Link className="albums-list__cover-container">
-                <img
-                  src="/img/gallery/album-selector/photo/cover-3.webp"
-                  alt=""
-                  className="albums-list__cover"
-                />
-              </Link>
-              <Link className="albums-list__link link-3">Фото с региона</Link>
-            </li>
-          </ul>
+          <ul className="albums-list">{albumListItems}</ul>
         </div>
       </>
     );
