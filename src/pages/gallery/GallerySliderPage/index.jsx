1c4ec781346399b6768d41558a601c62e7ea23b9
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-11 23:52:56 +0500

    inject store in slider page

--- src/pages/gallery/GallerySliderPage/index.jsx
+++ src/pages/gallery/GallerySliderPage/index.jsx
@@ -1,3 +1,4 @@
+import { inject, observer } from "mobx-react";
 import { useRef } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -16,150 +17,155 @@ import { useDocumentTitle } from "../../../hooks/useDocumentTitle";
 
 import "./index.css";
 
-const GallerySliderPage = () => {
-  const location = useLocation().pathname;
-  const documentTitle = location.endsWith("favorites")
-    ? "Избранное"
-    : "Галерея, просмотр альбома";
+const GallerySliderPage = inject(
+  "GalleryStore",
+  "UsersStore",
+)(
+  observer(({ GalleryStore }) => {
+    const location = useLocation().pathname;
+    const documentTitle = location.endsWith("favorites")
+      ? "Избранное"
+      : "Галерея, просмотр альбома";
 
-  useDocumentTitle(documentTitle);
+    useDocumentTitle(documentTitle);
 
-  const refs = useRef([]);
+    const refs = useRef([]);
 
-  const addToRefs = (el) => {
-    if (el && !refs.current.includes(el)) {
-      refs.current.push(el);
-    }
-  };
+    const addToRefs = (el) => {
+      if (el && !refs.current.includes(el)) {
+        refs.current.push(el);
+      }
+    };
 
-  return (
-    <>
-      <GalleryNavigator className="favorites__gallery-navigator-mb" />
-      <Swiper
-        autoHeight={false}
-        slidesPerView={1}
-        spaceBetween={30}
-        loop={true}
-        navigation={true}
-        modules={[Navigation]}
-        className="gallery-slider"
-        onSlideChange={() => {
-          refs.current.forEach((video) => {
-            video.pause();
-          });
-        }}
-      >
-        <SwiperSlide className="gallery-slider__slide">
-          <div className="gallery-slider__video-bg"></div>
-          <video controls className="gallery-slider__picture" ref={addToRefs}>
-            <source src="/gallery/video/region1/promo.mp4" type="video/mp4" />
-          </video>
-          <div className="gallery-slider__panel">
-            <button className="gallery-slider__button-like">
-              <Heart className="heart" />
-            </button>
-            <button className="gallery-slider__button-close">
-              <Cross className="cross" />
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="gallery-slider__slide">
-          <img
-            src="/gallery/photo/region1/1.webp"
-            alt=""
-            className="gallery-slider__image-bg"
-          />
-          <img
-            src="/gallery/photo/region1/1.webp"
-            alt=""
-            className="gallery-slider__picture"
-          />
-          <div className="gallery-slider__panel">
-            <button className="gallery-slider__button-like">
-              <Heart className="heart" />
-            </button>
-            <button className="gallery-slider__button-close">
-              <Cross className="cross" />
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="gallery-slider__slide">
-          <img
-            src="/gallery/photo/region1/2.webp"
-            alt=""
-            className="gallery-slider__image-bg"
-          />
-          <img
-            src="/gallery/photo/region1/2.webp"
-            alt=""
-            className="gallery-slider__picture"
-          />
-          <div className="gallery-slider__panel">
-            <button className="gallery-slider__button-like">
-              <Heart className="heart" />
-            </button>
-            <button className="gallery-slider__button-close">
-              <Cross className="cross" />
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="gallery-slider__slide">
-          <img
-            src="/gallery/photo/region1/3.webp"
-            alt=""
-            className="gallery-slider__image-bg"
-          />
-          <img
-            src="/gallery/photo/region1/3.webp"
-            alt=""
-            className="gallery-slider__picture"
-          />
-          <div className="gallery-slider__panel">
-            <button className="gallery-slider__button-like">
-              <Heart className="heart" />
-            </button>
-            <button className="gallery-slider__button-close">
-              <Cross className="cross" />
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="gallery-slider__slide">
-          <img
-            src="/gallery/photo/region1/4.webp"
-            alt=""
-            className="gallery-slider__image-bg"
-          />
-          <img
-            src="/gallery/photo/region1/4.webp"
-            alt=""
-            className="gallery-slider__picture"
-          />
-          <div className="gallery-slider__panel">
-            <button className="gallery-slider__button-like">
-              <Heart className="heart" />
-            </button>
-            <button className="gallery-slider__button-close">
-              <Cross className="cross" />
-            </button>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="gallery-slider__slide">
-          <div className="gallery-slider__video-bg"></div>
-          <video controls className="gallery-slider__picture" ref={addToRefs}>
-            <source src="/gallery/video/region1/tnt.mp4" type="video/mp4" />
-          </video>
-          <div className="gallery-slider__panel">
-            <button className="gallery-slider__button-like">
-              <Heart className="heart" />
-            </button>
-            <button className="gallery-slider__button-close">
-              <Cross className="cross" />
-            </button>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </>
-  );
-};
+    return (
+      <>
+        <GalleryNavigator className="favorites__gallery-navigator-mb" />
+        <Swiper
+          autoHeight={false}
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          navigation={true}
+          modules={[Navigation]}
+          className="gallery-slider"
+          onSlideChange={() => {
+            refs.current.forEach((video) => {
+              video.pause();
+            });
+          }}
+        >
+          <SwiperSlide className="gallery-slider__slide">
+            <div className="gallery-slider__video-bg"></div>
+            <video controls className="gallery-slider__picture" ref={addToRefs}>
+              <source src="/gallery/video/region1/promo.mp4" type="video/mp4" />
+            </video>
+            <div className="gallery-slider__panel">
+              <button className="gallery-slider__button-like">
+                <Heart className="heart" />
+              </button>
+              <button className="gallery-slider__button-close">
+                <Cross className="cross" />
+              </button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="gallery-slider__slide">
+            <img
+              src="/gallery/photo/region1/1.webp"
+              alt=""
+              className="gallery-slider__image-bg"
+            />
+            <img
+              src="/gallery/photo/region1/1.webp"
+              alt=""
+              className="gallery-slider__picture"
+            />
+            <div className="gallery-slider__panel">
+              <button className="gallery-slider__button-like">
+                <Heart className="heart" />
+              </button>
+              <button className="gallery-slider__button-close">
+                <Cross className="cross" />
+              </button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="gallery-slider__slide">
+            <img
+              src="/gallery/photo/region1/2.webp"
+              alt=""
+              className="gallery-slider__image-bg"
+            />
+            <img
+              src="/gallery/photo/region1/2.webp"
+              alt=""
+              className="gallery-slider__picture"
+            />
+            <div className="gallery-slider__panel">
+              <button className="gallery-slider__button-like">
+                <Heart className="heart" />
+              </button>
+              <button className="gallery-slider__button-close">
+                <Cross className="cross" />
+              </button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="gallery-slider__slide">
+            <img
+              src="/gallery/photo/region1/3.webp"
+              alt=""
+              className="gallery-slider__image-bg"
+            />
+            <img
+              src="/gallery/photo/region1/3.webp"
+              alt=""
+              className="gallery-slider__picture"
+            />
+            <div className="gallery-slider__panel">
+              <button className="gallery-slider__button-like">
+                <Heart className="heart" />
+              </button>
+              <button className="gallery-slider__button-close">
+                <Cross className="cross" />
+              </button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="gallery-slider__slide">
+            <img
+              src="/gallery/photo/region1/4.webp"
+              alt=""
+              className="gallery-slider__image-bg"
+            />
+            <img
+              src="/gallery/photo/region1/4.webp"
+              alt=""
+              className="gallery-slider__picture"
+            />
+            <div className="gallery-slider__panel">
+              <button className="gallery-slider__button-like">
+                <Heart className="heart" />
+              </button>
+              <button className="gallery-slider__button-close">
+                <Cross className="cross" />
+              </button>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="gallery-slider__slide">
+            <div className="gallery-slider__video-bg"></div>
+            <video controls className="gallery-slider__picture" ref={addToRefs}>
+              <source src="/gallery/video/region1/tnt.mp4" type="video/mp4" />
+            </video>
+            <div className="gallery-slider__panel">
+              <button className="gallery-slider__button-like">
+                <Heart className="heart" />
+              </button>
+              <button className="gallery-slider__button-close">
+                <Cross className="cross" />
+              </button>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </>
+    );
+  }),
+);
 
 export default GallerySliderPage;
