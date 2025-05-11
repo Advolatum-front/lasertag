1f2c880e9e3c8a7f6c74fc2405e7529fbdfd313d
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-11 19:51:22 +0500

    fix styles and layout

--- src/pages/gallery/AlbumSelector/index.jsx
+++ src/pages/gallery/AlbumSelector/index.jsx
@@ -26,7 +26,9 @@ const AlbumSelector = inject("GalleryStore")(
       return (
         <>
           <GalleryNavigator className="gallery-navigator-mb" />
-          <NoData />
+          <div className="albums-list-wrapper">
+            <NoData />
+          </div>
         </>
       );
     }
@@ -34,38 +36,40 @@ const AlbumSelector = inject("GalleryStore")(
     return (
       <>
         <GalleryNavigator className="gallery-navigator-mb" />
-        <ul className="albums-list">
-          <li className="albums-list__item">
-            <Link className="albums-list__cover-container">
-              <img
-                src="/img/gallery/album-selector/photo/cover-1.webp"
-                alt=""
-                className="albums-list__cover"
-              />
-            </Link>
-            <Link className="albums-list__link link-1">Фото с региона</Link>
-          </li>
-          <li className="albums-list__item">
-            <Link className="albums-list__cover-container">
-              <img
-                src="/img/gallery/album-selector/photo/cover-2.webp"
-                alt=""
-                className="albums-list__cover"
-              />
-            </Link>
-            <Link className="albums-list__link link-2">Фото с области</Link>
-          </li>
-          <li className="albums-list__item">
-            <Link className="albums-list__cover-container">
-              <img
-                src="/img/gallery/album-selector/photo/cover-3.webp"
-                alt=""
-                className="albums-list__cover"
-              />
-            </Link>
-            <Link className="albums-list__link link-3">Фото с региона</Link>
-          </li>
-        </ul>
+        <div className="albums-list-wrapper">
+          <ul className="albums-list">
+            <li className="albums-list__item">
+              <Link className="albums-list__cover-container">
+                <img
+                  src="/img/gallery/album-selector/photo/cover-1.webp"
+                  alt=""
+                  className="albums-list__cover"
+                />
+              </Link>
+              <Link className="albums-list__link link-1">Фото с региона</Link>
+            </li>
+            <li className="albums-list__item">
+              <Link className="albums-list__cover-container">
+                <img
+                  src="/img/gallery/album-selector/photo/cover-2.webp"
+                  alt=""
+                  className="albums-list__cover"
+                />
+              </Link>
+              <Link className="albums-list__link link-2">Фото с области</Link>
+            </li>
+            <li className="albums-list__item">
+              <Link className="albums-list__cover-container">
+                <img
+                  src="/img/gallery/album-selector/photo/cover-3.webp"
+                  alt=""
+                  className="albums-list__cover"
+                />
+              </Link>
+              <Link className="albums-list__link link-3">Фото с региона</Link>
+            </li>
+          </ul>
+        </div>
       </>
     );
   }),
