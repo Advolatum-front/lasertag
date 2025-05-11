452759ffe99c8919dd15f213751e5cf40e109617
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-11 16:35:49 +0500

    inject gallery store in album selector

--- src/pages/gallery/AlbumSelector/index.jsx
+++ src/pages/gallery/AlbumSelector/index.jsx
@@ -1,3 +1,4 @@
+import { inject, observer } from "mobx-react";
 import { Link } from "react-router-dom";
 
 import GalleryNavigator from "../../../components/GalleryNavigator";
@@ -6,46 +7,48 @@ import { useDocumentTitle } from "../../../hooks/useDocumentTitle";
 
 import "./index.css";
 
-const AlbumSelector = () => {
-  useDocumentTitle("Галерея, альбомы");
+const AlbumSelector = inject("GalleryStore")(
+  observer(({ GalleryStore }) => {
+    useDocumentTitle("Галерея, альбомы");
 
-  return (
-    <>
-      <GalleryNavigator className="gallery-navigator-mb" />
-      <ul className="albums-list">
-        <li className="albums-list__item">
-          <Link className="albums-list__cover-container">
-            <img
-              src="/img/gallery/album-selector/photo/cover-1.webp"
-              alt=""
-              className="albums-list__cover"
-            />
-          </Link>
-          <Link className="albums-list__link link-1">Фото с региона</Link>
-        </li>
-        <li className="albums-list__item">
-          <Link className="albums-list__cover-container">
-            <img
-              src="/img/gallery/album-selector/photo/cover-2.webp"
-              alt=""
-              className="albums-list__cover"
-            />
-          </Link>
-          <Link className="albums-list__link link-2">Фото с области</Link>
-        </li>
-        <li className="albums-list__item">
-          <Link className="albums-list__cover-container">
-            <img
-              src="/img/gallery/album-selector/photo/cover-3.webp"
-              alt=""
-              className="albums-list__cover"
-            />
-          </Link>
-          <Link className="albums-list__link link-3">Фото с региона</Link>
-        </li>
-      </ul>
-    </>
-  );
-};
+    return (
+      <>
+        <GalleryNavigator className="gallery-navigator-mb" />
+        <ul className="albums-list">
+          <li className="albums-list__item">
+            <Link className="albums-list__cover-container">
+              <img
+                src="/img/gallery/album-selector/photo/cover-1.webp"
+                alt=""
+                className="albums-list__cover"
+              />
+            </Link>
+            <Link className="albums-list__link link-1">Фото с региона</Link>
+          </li>
+          <li className="albums-list__item">
+            <Link className="albums-list__cover-container">
+              <img
+                src="/img/gallery/album-selector/photo/cover-2.webp"
+                alt=""
+                className="albums-list__cover"
+              />
+            </Link>
+            <Link className="albums-list__link link-2">Фото с области</Link>
+          </li>
+          <li className="albums-list__item">
+            <Link className="albums-list__cover-container">
+              <img
+                src="/img/gallery/album-selector/photo/cover-3.webp"
+                alt=""
+                className="albums-list__cover"
+              />
+            </Link>
+            <Link className="albums-list__link link-3">Фото с региона</Link>
+          </li>
+        </ul>
+      </>
+    );
+  }),
+);
 
 export default AlbumSelector;
--- src/stores/galleryStore.js
+++ src/stores/galleryStore.js
@@ -4,10 +4,19 @@ class GalleryStore {
   initialMediaList = require("./data/gallery.json");
 
   mediaList = [];
+  albumsList = [];
 
   constructor() {
-    makeObservable(this, {});
+    makeObservable(this, {
+      albumsList: observable,
+    });
   }
+
+  fetchAlbumsByType = (type, limit = 3) => {
+    this.albumsList = this.initialMediaList
+      .filter((album) => album.type === type)
+      .slice(0, limit);
+  };
 }
 
 const galleryStore = new GalleryStore();
