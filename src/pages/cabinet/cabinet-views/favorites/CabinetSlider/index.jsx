19e3126ef44d38c9d52fbec70b3498e321411a57
Advolatum-front <Advolatum-front@yandex.ru>
2025-05-13 04:17:30 +0500

    all done

--- src/pages/cabinet/cabinet-views/favorites/CabinetSlider/index.jsx
+++ src/pages/cabinet/cabinet-views/favorites/CabinetSlider/index.jsx
@@ -50,26 +50,27 @@ const CabinetSlider = inject("UsersStore")(
       }
     };
 
-    /*const urlLink = `/cabiet/favorites/${filterValue}/${id}`;*/
+    /*const urlLink = `/cabinet/favorites/${filterValue}/${id}`;*/
 
     const swiperSlides = dataArray.map((item) => {
       const { id, type, src } = item;
-      const slideContent = (itemsType = "photo" ? (
-        <>
-          <img src={src} alt="" className="cabinet-slider__image-bg" />
-          <img src={src} alt="" className="cabinet-slider__picture" />
-        </>
-      ) : (
-        <>
-          <div className="cabinet-slider__video-bg" />
-          <video controls className="cabinet-slider__picture" ref={addToRefs}>
-            <source src={src} type="video/mp4" />
-          </video>
-        </>
-      ));
+      const slideContent =
+        type === "photo" ? (
+          <>
+            <img src={src} alt="" className="cabinet-slider__image-bg" />
+            <img src={src} alt="" className="cabinet-slider__picture" />
+          </>
+        ) : (
+          <>
+            <div className="cabinet-slider__video-bg" />
+            <video controls className="cabinet-slider__picture" ref={addToRefs}>
+              <source src={src} type="video/mp4" />
+            </video>
+          </>
+        );
 
       return (
-        <SwiperSlide className="cabinet-slider__slide">
+        <SwiperSlide className="cabinet-slider__slide" key={id}>
           {slideContent}
           <button
             className="cabinet-slider__button-unlike"
@@ -114,54 +115,7 @@ const CabinetSlider = inject("UsersStore")(
             swiper.navigation.update();
           }}
         >
-          <SwiperSlide className="cabinet-slider__slide">
-            <div className="cabinet-slider__video-bg"></div>
-            <video controls className="cabinet-slider__picture" ref={addToRefs}>
-              <source src="/gallery/video/region1/promo.mp4" type="video/mp4" />
-            </video>
-            <button className="cabinet-slider__button-unlike">
-              <Cross className="cabinet-slider__cross-ico" />
-            </button>
-          </SwiperSlide>
-          <SwiperSlide className="cabinet-slider__slide">
-            <img
-              src="/gallery/photo/region1/1.webp"
-              alt=""
-              className="cabinet-slider__image-bg"
-            />
-            <img
-              src="/gallery/photo/region1/1.webp"
-              alt=""
-              className="cabinet-slider__picture"
-            />
-            <button className="cabinet-slider__button-unlike">
-              <Cross className="cabinet-slider__cross-ico" />
-            </button>
-          </SwiperSlide>
-          <SwiperSlide className="cabinet-slider__slide">
-            <img
-              src="/gallery/photo/region1/2.webp"
-              alt=""
-              className="cabinet-slider__image-bg"
-            />
-            <img
-              src="/gallery/photo/region1/2.webp"
-              alt=""
-              className="cabinet-slider__picture"
-            />
-            <button className="cabinet-slider__button-unlike">
-              <Cross className="cabinet-slider__cross-ico" />
-            </button>
-          </SwiperSlide>
-          <SwiperSlide className="cabinet-slider__slide">
-            <div className="cabinet-slider__video-bg"></div>
-            <video controls className="cabinet-slider__picture" ref={addToRefs}>
-              <source src="/gallery/video/region1/tnt.mp4" type="video/mp4" />
-            </video>
-            <button className="cabinet-slider__button-unlike">
-              <Cross className="cabinet-slider__cross-ico" />
-            </button>
-          </SwiperSlide>
+          {swiperSlides}
         </Swiper>
         <div className="custom-navigation cabinet-slider-nav">
           <button className="cabinet-slider__prev"></button>
